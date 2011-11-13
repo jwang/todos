828d85132170f7227a303be21aac003ce2c06df7
John Wang <john@johntwang.com>
2011-11-13 10:33:03 -1000

    set up the create new todo method

--- apps/todos/todos.js
+++ apps/todos/todos.js
@@ -14,6 +14,12 @@ Todos.Todo = SC.Object.extend({
 Todos.todoListController = SC.ArrayController.create({
   // Initialize the array controller with an empty array.
   content: []
+
+  // Creates a new todo with the passed title, then adds it
+  // to the array.
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
 });
 
 SC.ready(function() {
