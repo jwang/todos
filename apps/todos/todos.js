29dbd8a5455bcf40f8a625c9847fda1735abfedc
John Wang <john@johntwang.com>
2011-11-13 13:57:26 -1000

    mark all as done

--- apps/todos/todos.js
+++ apps/todos/todos.js
@@ -28,7 +28,17 @@ Todos.todoListController = SC.ArrayController.create({
 
   clearCompletedTodos: function() {
     this.filterProperty('isDone', true).forEach(this.removeObject, this);
-  }
+  },
+
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+
+      return value;
+    } else {
+      return this.get('length') && this.everyProperty('isDone', true);
+    }
+  }.property('@each.isDone')
 });
 
 Todos.CreateTodoView = SC.TextField.extend({
