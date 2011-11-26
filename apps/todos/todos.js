1d077cb025b76d4ebaf2f2330316bc6deef44699
John Wang <john@johntwang.com>
2011-11-25 16:48:30 -1000

    Jasmine doesn't like the remaining and allAreDone methods
    stack trace is nasty.
    
    https://gist.github.com/1394868

--- apps/todos/todos.js
+++ apps/todos/todos.js
@@ -22,23 +22,23 @@ Todos.todoListController = SC.ArrayController.create({
     this.pushObject(todo);
   },
 
-  remaining: function() {
-    return this.filterProperty('isDone', false).get('length');
-  }.property('@each.isDone'),
+//  remaining: function() {
+//    return this.filterProperty('isDone', false).get('length');
+//  }.property('@each.isDone'),
 
   clearCompletedTodos: function() {
     this.filterProperty('isDone', true).forEach(this.removeObject, this);
-  },
+  }//,
 
-  allAreDone: function(key, value) {
-    if (value !== undefined) {
-      this.setEach('isDone', value);
+//  allAreDone: function(key, value) {
+//    if (value !== undefined) {
+//      this.setEach('isDone', value);
 
-      return value;
-    } else {
-      return this.get('length') && this.everyProperty('isDone', true);
-    }
-  }.property('@each.isDone')
+//      return value;
+//    } else {
+//      return this.get('length') && this.everyProperty('isDone', true);
+//    }
+//  }.property('@each.isDone')
 });
 
 Todos.CreateTodoView = SC.TextField.extend({
