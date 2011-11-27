bf1db495b287ddedc81984b6cae7d23a6f9fc164
John Wang <john@johntwang.com>
2011-11-26 21:04:53 -1000

    Add a spec for the controller's remaining function

--- apps/todos/tests/unit/controllers/todo_list_controller_spec.js
+++ apps/todos/tests/unit/controllers/todo_list_controller_spec.js
@@ -26,5 +26,38 @@ describe('Todos.todoListController', function() {
       expect(Todos.todoListController.get('content')).toContain(todo);
     });
   });
+
+  describe("remaining", function() {
+    var createTodoSpy, title, firstTodo, secondTodo, todoHash;
+    beforeEach(function() {
+      
+      // clear any previous todos created
+      var length = Todos.todoListController.get('content').length;
+      var i = 0;
+      for (i = 0 ; i < length; i++) {
+        var removeTodo = Todos.todoListController.get('content').get(i);
+        Todos.todoListController.removeObject(removeTodo);
+      }
+      
+      title = 'title';
+      todoHash = {title: title};
+      createTodoSpy = spyOn(Todos.Todo, 'create').andCallThrough();
+      Todos.todoListController.createTodo(title);
+      Todos.todoListController.createTodo(title);
+      firstTodo = Todos.todoListController.get('content').get(0);
+      secondTodo = Todos.todoListController.get('content').get(1);
+    });
+
+    afterEach(function () {
+      // clean up after each spec run
+      Todos.todoListController.removeObject(firstTodo);
+      Todos.todoListController.removeObject(secondTodo);
+    });
+
+    it("should calculate number of remaining items", function() {
+      expect(Todos.todoListController.remaining()).toEqual(2);
+    });
+
+  });
 });
 
--- apps/todos/tests/unit/views/create_todo_view_spec.js
+++ apps/todos/tests/unit/views/create_todo_view_spec.js
@@ -4,12 +4,18 @@ describe('Todos.CreateTodoView', function() {
   });
 
   describe('#insertNewline', function() {
-    var createTodoSpy, value;
+    var createTodoSpy, value, todo;
     beforeEach(function() {
       value = 'Do Something';
       view = Todos.CreateTodoView.create({value: value});
       createTodoSpy = spyOn(Todos.todoListController, 'createTodo');
       view.insertNewline();
+      todo = Todos.todoListController.get('content').get(0);
+    });
+
+    afterEach(function () {
+      // clean up after each spec run
+      Todos.todoListController.removeObject(todo);
     });
 
     it('delegates to create a new todo with it\'s current value', function() {
