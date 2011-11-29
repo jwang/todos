fa3ea57777403b21825db68333de61c67e27da3c
John Wang <john@johntwang.com>
2011-11-29 00:06:39 -1000

    Tag Feature

--- apps/todos/views/stats_view.js
+++ apps/todos/views/stats_view.js
@@ -4,5 +4,5 @@ Todos.StatsView = SC.TemplateView.extend({
   displayRemaining: function() {
     var remaining = this.get('remaining');
     return remaining + (remaining === 1 ? " item" : " items");
-  }.property('remaining').cacheable()
+  }.property('remaining')
 });
